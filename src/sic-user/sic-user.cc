f0eadc57e30b75742bee9730841099a508eaf8b1
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2020-10-06 22:43:25 -0700

    clean up main

--- src/sic-user/sic-user.cc
+++ src/sic-user/sic-user.cc
@@ -12,20 +12,18 @@
 #include <vector>
 #include <windows.h>
 
-#if defined(__i386__) || defined(_M_IX86)
-#define SIC_ARCH "x86"
-#elif defined(__amd64__) || defined(_M_X64)
-#define SIC_ARCH "x64"
-#else
-#error Platform not supported.
-#endif
-
-namespace fs = std::filesystem;
+//
+// Various defines.
+//
 
 const char *ServiceName = "sic";
 const char *ServiceDisplayName = "Sharing Is Caring Driver";
 const char *ServiceFilename = "sic-drv.sys";
 const char *DeviceName = R"(\\.\)" SIC_DEVICE_NAME;
+
+//
+// Options.
+//
 
 struct Opts_t {
   std::string Filter;
@@ -51,70 +49,6 @@ int main(int argc, char *argv[]) {
     RemoveDriver(ServiceName);
   });
 
-  //
-  // Ensure that we both have dbghelp.dll and symsrv.dll in the current
-  // directory otherwise things don't work. cf
-  // https://docs.microsoft.com/en-us/windows/win32/debug/using-symsrv
-  // "Installation"
-  //
-
-  char ExePathBuffer[MAX_PATH];
-  if (!GetModuleFileNameA(nullptr, &ExePathBuffer[0], sizeof(ExePathBuffer))) {
-    printf("GetModuleFileNameA failed.\n");
-    return EXIT_FAILURE;
-  }
-
-  //
-  // Let's check if the two dlls exist in the same path as the application.
-  //
-
-  const fs::path ExePath(ExePathBuffer);
-  const fs::path ParentDir(ExePath.parent_path());
-  if (!fs::exists(ParentDir / "dbghelp.dll") ||
-      !fs::exists(ParentDir / "symsrv.dll")) {
-    //
-    // Apparently they don't. Be nice and try to find them by ourselves.
-    //
-
-    const fs::path DefaultDbghelpLocation(
-        R"(c:\program Files (x86)\windows kits\10\debuggers\)" SIC_ARCH
-        R"(\dbghelp.dll)");
-    const fs::path DefaultSymsrvLocation(
-        R"(c:\program Files (x86)\windows kits\10\debuggers\)" SIC_ARCH
-        R"(\symsrv.dll)");
-
-    const bool Dbghelp = fs::exists(DefaultDbghelpLocation);
-    const bool Symsrv = fs::exists(DefaultSymsrvLocation);
-
-    //
-    // If they don't exist and we haven't them ourselves, then we have to
-    // exit.
-    //
-
-    if (!Dbghelp || !Symsrv) {
-      printf("The debugger class expects dbghelp.dll / symsrv.dll in the "
-             "directory "
-             "where the application is running from.\n");
-      return EXIT_FAILURE;
-    }
-
-    //
-    // Sounds like we are able to fix the problem ourselves. Copy the files in
-    // the directory where the application is running from and move on!
-    //
-
-    fs::copy(DefaultDbghelpLocation, ParentDir);
-    fs::copy(DefaultSymsrvLocation, ParentDir);
-    printf("Copied dbghelp and symsrv.dll from default location into the "
-           "executable directory..\n");
-  }
-
-  //
-  // Initialize dbghelp.
-  //
-
-  const ScopedSymInit Sym(SYMOPT_CASE_INSENSITIVE | SYMOPT_UNDNAME);
-
   SIC_OFFSETS SicOffsets;
   RtlZeroMemory(&SicOffsets, sizeof(SicOffsets));
 
@@ -122,24 +56,8 @@ int main(int argc, char *argv[]) {
   // Grab offsets.
   //
 
-  if (!GetFieldOffsetFromModule(LR"(c:\windows\system32\ntoskrnl.exe)",
-                                L"_EPROCESS", L"VadRoot",
-                                &SicOffsets.EPROCESSToVadRoot)) {
-    printf("Failed to grab nt!_EPROCESS.VadRoot offset.\n");
-    return EXIT_FAILURE;
-  }
-
-  if (!GetFieldOffsetFromModule(LR"(c:\windows\system32\ntoskrnl.exe)",
-                                L"_MMVAD_SHORT", L"u",
-                                &SicOffsets.MMVAD_SHORTToVadFlags)) {
-    printf("Failed to grab nt!_MMVAD_SHORT.u.VadFlags offset.\n");
-    return EXIT_FAILURE;
-  }
-
-  if (!GetFieldOffsetFromModule(
-          LR"(c:\windows\system32\ntoskrnl.exe)", L"_MMVAD_FLAGS",
-          L"PrivateMemory", &SicOffsets.MMVAD_FLAGSPrivateMemoryBitPosition)) {
-    printf("Failed to grab nt!_MMVAD_FLAGS.PrivateMemory bit position.\n");
+  if (!GetOffsets(SicOffsets)) {
+    printf("Failed to grab offsets.\n");
     return EXIT_FAILURE;
   }
 
--- src/sic-user/utils.cc
+++ src/sic-user/utils.cc
@@ -1,11 +1,23 @@
 // Axel '0vercl0k' Souchet - October 6 2020
 #include "utils.h"
+#include "sym.h"
 #include <algorithm>
 #include <cctype>
+#include <filesystem>
 #include <memory>
 #include <windows.h>
 
+namespace fs = std::filesystem;
+
 #pragma comment(lib, "ntdll")
+
+#if defined(__i386__) || defined(_M_IX86)
+#define SIC_ARCH "x86"
+#elif defined(__amd64__) || defined(_M_X64)
+#define SIC_ARCH "x64"
+#else
+#error Platform not supported.
+#endif
 
 //
 // Thanks to ProcessHacker's native library:
@@ -154,4 +166,99 @@ std::unordered_map<uintptr_t, std::string> GetProcessList() {
   //
 
   return Processes;
+}
+
+bool GetOffsets(SIC_OFFSETS &SicOffsets) {
+
+  //
+  // Ensure that we both have dbghelp.dll and symsrv.dll in the current
+  // directory otherwise things don't work. cf
+  // https://docs.microsoft.com/en-us/windows/win32/debug/using-symsrv
+  // "Installation"
+  //
+
+  char ExePathBuffer[MAX_PATH];
+  if (!GetModuleFileNameA(nullptr, &ExePathBuffer[0], sizeof(ExePathBuffer))) {
+    printf("GetModuleFileNameA failed.\n");
+    return false;
+  }
+
+  //
+  // Let's check if the two dlls exist in the same path as the application.
+  //
+
+  const fs::path ExePath(ExePathBuffer);
+  const fs::path ParentDir(ExePath.parent_path());
+  if (!fs::exists(ParentDir / "dbghelp.dll") ||
+      !fs::exists(ParentDir / "symsrv.dll")) {
+
+    //
+    // Apparently they don't. Be nice and try to find them by ourselves.
+    //
+
+    const fs::path DefaultDbghelpLocation(
+        R"(c:\program Files (x86)\windows kits\10\debuggers\)" SIC_ARCH
+        R"(\dbghelp.dll)");
+    const fs::path DefaultSymsrvLocation(
+        R"(c:\program Files (x86)\windows kits\10\debuggers\)" SIC_ARCH
+        R"(\symsrv.dll)");
+
+    const bool Dbghelp = fs::exists(DefaultDbghelpLocation);
+    const bool Symsrv = fs::exists(DefaultSymsrvLocation);
+
+    //
+    // If they don't exist and we haven't them ourselves, then we have to
+    // exit.
+    //
+
+    if (!Dbghelp || !Symsrv) {
+      printf("The debugger class expects dbghelp.dll / symsrv.dll in the "
+             "directory "
+             "where the application is running from.\n");
+      return false;
+    }
+
+    //
+    // Sounds like we are able to fix the problem ourselves. Copy the files in
+    // the directory where the application is running from and move on!
+    //
+
+    fs::copy(DefaultDbghelpLocation, ParentDir);
+    fs::copy(DefaultSymsrvLocation, ParentDir);
+    printf("Copied dbghelp and symsrv.dll from default location into the "
+           "executable directory..\n");
+  }
+
+  //
+  // Initialize dbghelp.
+  //
+
+  const ScopedSymInit Sym(SYMOPT_CASE_INSENSITIVE | SYMOPT_UNDNAME);
+
+  //
+  // Grab offsets off nt.
+  //
+
+  if (!GetFieldOffsetFromModule(LR"(c:\windows\system32\ntoskrnl.exe)",
+                                L"_EPROCESS", L"VadRoot",
+                                &SicOffsets.EPROCESSToVadRoot)) {
+    printf("Failed to grab nt!_EPROCESS.VadRoot offset.\n");
+    return false;
+  }
+
+  if (!GetFieldOffsetFromModule(LR"(c:\windows\system32\ntoskrnl.exe)",
+                                L"_MMVAD_SHORT", L"u",
+                                &SicOffsets.MMVAD_SHORTToVadFlags)) {
+    printf("Failed to grab nt!_MMVAD_SHORT.u.VadFlags offset.\n");
+    return false;
+  }
+
+  if (!GetFieldOffsetFromModule(
+          LR"(c:\windows\system32\ntoskrnl.exe)", L"_MMVAD_FLAGS",
+          L"PrivateMemory", &SicOffsets.MMVAD_FLAGSPrivateMemoryBitPosition)) {
+    printf("Failed to grab nt!_MMVAD_FLAGS.PrivateMemory bit position.\n");
+    return false;
+  }
+
+  return true;
 }
