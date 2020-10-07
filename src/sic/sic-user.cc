5cbd1e94eb603dad12ddb99b4649ce929c7ccc69
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2020-10-07 02:58:18 -0700

    rename a bit things

--- src/sic/sic-user.cc
+++ src/sic/sic-user.cc
@@ -57,8 +57,27 @@ int main(int argc, char *argv[]) {
   //
 
   if (!GetOffsets(SicOffsets)) {
-    printf("Failed to grab offsets.\n");
-    return EXIT_FAILURE;
+
+    //
+    // If we failed to grab offsets, let's try again by setting a dummy
+    // _NT_SYMBOL_PATH.
+    //
+
+    printf("Failed to GetOffsets but let's try again with a dummy "
+           "_NT_SYMBOL_PATH..\n");
+
+    const bool Res = SetEnvironmentVariableA("_NT_SYMBOL_PATH", "srv*");
+    if (!Res || !GetOffsets(SicOffsets)) {
+
+      //
+      // This time we really call it quits.
+      //
+
+      printf("Failed to grab offsets.\n");
+      return EXIT_FAILURE;
+    }
+
+    printf("This fixed the issue, ignore the above error messages :)\n");
   }
 
   //
